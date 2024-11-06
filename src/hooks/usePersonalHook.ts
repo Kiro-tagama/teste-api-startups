0c59f18ac121c68ec2a89fedd09d6d0b586875c8
Rodrigo Lopes <59925590+Kiro-tagama@users.noreply.github.com>
2024-11-06 14:52:21 +0000

    organize folders

---
+++ src/hooks/usePersonalHook.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { generateStartupIdea, validateApiKey } from "../api/openIaApi";
+
+const envApiKey = import.meta.env.VITE_API_KEY ?? "";
+
+export interface MessageProps{
+  id: "user" | "bot";
+  message: string;
+}
+
+export function usePersonalHook(){
+  const [apiKey,setApiKey] = useState<string>(envApiKey);
+  const [validApiKey,setValidApiKey] = useState<boolean>(false);
+
+  const [messages,setMessages] = useState<MessageProps[]|[]>([]);
+
+  async function getStatusValidateApi () {setValidApiKey(await validateApiKey(apiKey))}
+  useEffect(()=>{
+    getStatusValidateApi()
+  },[apiKey])
+  
+  function sendMessage(message:string) {
+    setMessages([...messages, {id:"user", message:message}])
+
+    generateStartupIdea(apiKey,message)
+      .then(idea =>{
+        setMessages([...messages, {id:"bot", message:idea}])
+      })
+      .catch(error => console.error("Erro:", error)); 
+  }
+
+  return{
+    apiKey,setApiKey,validApiKey,
+    sendMessage,messages
+  }
+}
